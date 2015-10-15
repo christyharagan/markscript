f3bcd320be87f36a45b9e2c75c41eb50dddfa9fc
Christy Haragan <christy.haragan.github@gmail.com>
2015-10-15 16:17:51 +0100

    Rename cli to markscript

--- src/bin/markscript.ts
+++ src/bin/markscript.ts
@@ -98,7 +98,7 @@ cli.launch({}, function(env) {
     // LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
     // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
     // SOFTWARE.
-    
+
     let sanitizeOptions = {
       replacement: "_"
     }
